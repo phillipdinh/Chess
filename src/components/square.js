2acdfeb0003b4ce21102f3c63912ebbbfd729954
phillipdinh <phillipdinh11@yahoo.com>
2024-08-06 14:32:08 -0700

    Removed individual piece components

--- src/components/square.js
+++ src/components/square.js
@@ -1,27 +1,23 @@
-import King from "./pieces/king"
-import Queen from "./pieces/queen"
-import Rook from "./pieces/rook"
-import Bishop from "./pieces/bishop"
-import Knight from "./pieces/knight"
-import Pawn from "./pieces/pawn"
+import Piece from "./piece"
 
 export default function Square(props) {
+	//TODO highlight selected
 	const id = `sq-${props.col}-${props.row}`
 
 	const renderPiece = (piece, color, onClick) => {
 		switch (piece) {
 			case "k":
-				return <King color={color} onClick={onClick} />
+				return <Piece color={color} icon="♔" />
 			case "q":
-				return <Queen color={color} onClick={onClick} />
+				return <Piece color={color} icon="♕" />
 			case "r":
-				return <Rook color={color} onClick={onClick} />
+				return <Piece color={color} icon="♖" />
 			case "b":
-				return <Bishop color={color} onClick={onClick} />
+				return <Piece color={color} icon="♗" />
 			case "n":
-				return <Knight color={color} onClick={onClick} />
+				return <Piece color={color} icon="♞" />
 			case "p":
-				return <Pawn color={color} onClick={onClick} />
+				return <Piece color={color} icon="♙" />
 			default:
 				return null // No piece
 		}
@@ -31,17 +27,11 @@ export default function Square(props) {
 	return (
 		<div
 			id={id}
-			className={`board-square ${
-				(props.row + props.col) % 2 === 0 ? "light" : "dark"
-			}`}
+			className={`board-square ${(props.row + props.col) % 2 === 0 ? "light" : "dark"}`}
 			onClick={props.onClick}
 		>
-			{showRow ? (
-				<span className="board-label row-label">{`${props.row}`}</span>
-			) : null}
-			{showCol ? (
-				<span className="board-label col-label">{`${props.col}`}</span>
-			) : null}
+			{showRow ? <span className="board-label row-label">{`${props.row}`}</span> : null}
+			{showCol ? <span className="board-label col-label">{`${props.col}`}</span> : null}
 			{renderPiece(props.piece, props.color, props.onClick)}
 		</div>
 	)
