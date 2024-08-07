3079e597e6965096479fd05bdf3a7ad979d3c44c
phillipdinh <phillipdinh11@yahoo.com>
2024-08-07 09:15:17 -0700

    Added capture tally

--- src/components/gameInfo.js
+++ src/components/gameInfo.js
@@ -1,12 +1,36 @@
 import React, { useState } from "react"
 export default function GameInfo(props) {
 	/* TODO
-    - Whose turn
-    - How many turns
-    - Pieces captured
     - Game over
-    - Bad move indicator
     */
-
-	return <div>Turn: {props.turn ? "White" : "Black"} </div>
+	const pieces = {
+		k: "♔",
+		q: "♕",
+		r: "♖",
+		b: "♗",
+		n: "♞",
+		p: "♙"
+	}
+	return (
+		<div className="game-info">
+			<div>Turn: {props.turn ? "White" : "Black"} </div>
+			<div className="captured-pieces">
+				White Tally:
+				{props.whiteTally.map((square, index) => (
+					<div key={index} className={square.color}>
+						{pieces[square.piece]}
+					</div>
+				))}
+			</div>
+			<div className="captured-pieces">
+				Black Tally:
+				{props.blackTally.map((square, index) => (
+					<div key={index} className={square.color}>
+						{pieces[square.piece]}
+					</div>
+				))}
+			</div>
+			{props.badMove ? <div>Bad</div> : null}
+		</div>
+	)
 }
