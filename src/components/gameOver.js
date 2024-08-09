4499f58241976e3bb370919abf3fbd6a6c2c937d
phillipdinh <phillipdinh11@yahoo.com>
2024-08-09 12:46:58 -0700

    Added check mate pop up and play again button

--- src/components/gameOver.js
+++ src/components/gameOver.js
@@ -1,5 +1,14 @@
 import React from "react"
-//TODO add css
 export default function GameOver(props) {
-	return <div className="game-over">GAMEOVER</div>
+	return (
+		<>
+			<div className="screen-overlay"> </div>
+			<div className="game-over-container">
+				<p className="game-over-text">Checkmate</p>
+				<button className="new-game-btn" onClick={props.handlePlayAgain}>
+					Play Again?
+				</button>
+			</div>
+		</>
+	)
 }
