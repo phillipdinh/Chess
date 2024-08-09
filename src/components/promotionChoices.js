c432d0c34e66e3b80689361a90c9ef0c78ada0b9
phillipdinh <phillipdinh11@yahoo.com>
2024-08-09 06:33:50 -0700

    Added pawn promotion styles

--- src/components/promotionChoices.js
+++ src/components/promotionChoices.js
@@ -1,17 +1,28 @@
-import React, { useState } from "react"
+import React from "react"
 
-//TODO add css
 export default function PromotionChoices(props) {
 	const handlePromotion = (choice) => {
 		const square = props.square
 		props.onClick(square.row, square.col, choice, square.color)
 	}
+	const styles = {
+		left: `${props.square.col * 64}px`, // Adjust this based on your square size
+		top: `${props.square.row * 64}px` // Adjust this based on your square size
+	}
 	return (
-		<div className="pawn-promotion">
-			<div onClick={() => handlePromotion("q")}>♕</div>
-			<div onClick={() => handlePromotion("r")}>♖</div>
-			<div onClick={() => handlePromotion("b")}>♗</div>
-			<div onClick={() => handlePromotion("n")}>♞</div>
+		<div className="pawn-promotion" style={styles}>
+			<div className="promotion-choice" onClick={() => handlePromotion("q")}>
+				♕
+			</div>
+			<div className="promotion-choice" onClick={() => handlePromotion("r")}>
+				♖
+			</div>
+			<div className="promotion-choice" onClick={() => handlePromotion("b")}>
+				♗
+			</div>
+			<div className="promotion-choice" onClick={() => handlePromotion("n")}>
+				♞
+			</div>
 		</div>
 	)
 }
