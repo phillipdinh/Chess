716ee51e4f2bb81bb31961655f871f9bdbee2922
phillipdinh <phillipdinh11@yahoo.com>
2024-08-09 14:44:10 -0700

    Used mateStatus to render game over text

--- src/components/gameComponents/gameOver.js
+++ src/components/gameComponents/gameOver.js
@@ -4,7 +4,9 @@ export default function GameOver(props) {
 		<>
 			<div className="screen-overlay"> </div>
 			<div className="game-over-container">
-				<p className="game-over-text">Checkmate</p>
+				<p className="game-over-text">
+					{props.mateStatus === "checkmate" ? "Checkmate" : "Stalemate"}
+				</p>
 				<button className="new-game-btn" onClick={props.handlePlayAgain}>
 					Play Again?
 				</button>
